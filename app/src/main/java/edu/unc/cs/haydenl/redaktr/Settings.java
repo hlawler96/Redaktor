3ba4f85ba7e2cfe4df7e5ba827cb65a856f0bcdd
dbearded <derek.bearded@gmail.com>
2018-09-28 13:43:56 -0400

    Auth done

--- app/src/main/java/edu/unc/cs/haydenl/redaktr/Settings.java
+++ app/src/main/java/edu/unc/cs/haydenl/redaktr/Settings.java
@@ -6,31 +6,50 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 
+import static edu.unc.cs.haydenl.redaktr.SigninActivity.*;
+
 public class Settings extends AppCompatActivity {
 
-    private boolean personalInformation = false;
-    private boolean socialSecurity = false;
-    private boolean phoneNumber = false;
-    private boolean creditCard = false;
+    private boolean personalInformationSwitch = false;
+    private boolean socialSecuritySwitch = false;
+    private boolean phoneNumberSwitch = false;
+    private boolean creditCardSwitch = false;
+
+    private String displayName;
+    private String email;
+    private String phoneNumber;
+    private String uId;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            displayName = savedInstanceState.getString(DISPLAY_NAME);
+            email = savedInstanceState.getString(SigninActivity.EMAIL);
+            phoneNumber = savedInstanceState.getString(SigninActivity.PHONE_NUMBER);
+            uId = savedInstanceState.getString(SigninActivity.UID);
+        }
+
         setContentView(R.layout.activity_settings);
     }
 
     public void takePicture(View v){
-        personalInformation = ((Switch) findViewById(R.id.personalInformation)).isChecked();
-        socialSecurity = ((Switch) findViewById(R.id.socialSecurity)).isChecked();
-        phoneNumber = ((Switch) findViewById(R.id.phoneNumbers)).isChecked();
-        creditCard = ((Switch) findViewById(R.id.creditCard)).isChecked();
+        personalInformationSwitch = ((Switch) findViewById(R.id.personalInformation)).isChecked();
+        socialSecuritySwitch = ((Switch) findViewById(R.id.socialSecurity)).isChecked();
+        phoneNumberSwitch = ((Switch) findViewById(R.id.phoneNumbers)).isChecked();
+        creditCardSwitch = ((Switch) findViewById(R.id.creditCard)).isChecked();
 
         Intent redaktrIntent = new Intent(this, Redaktr.class);
-        redaktrIntent.putExtra(Redaktr.CREDIT_CARD, creditCard);
-        redaktrIntent.putExtra(Redaktr.PERSONAL_INFORMATION, personalInformation);
-        redaktrIntent.putExtra(Redaktr.PHONE_NUMBERS, phoneNumber);
-        redaktrIntent.putExtra(Redaktr.SOCIAL_SECURITY, socialSecurity);
+        redaktrIntent.putExtra(Redaktr.CREDIT_CARD_ENABLED, creditCardSwitch);
+        redaktrIntent.putExtra(Redaktr.PERSONAL_INFORMATION_ENABLED, personalInformationSwitch);
+        redaktrIntent.putExtra(Redaktr.PHONE_NUMBERS_ENABLED, phoneNumberSwitch);
+        redaktrIntent.putExtra(Redaktr.SOCIAL_SECURITY_ENABLED, socialSecuritySwitch);
+        redaktrIntent.putExtra(DISPLAY_NAME,  displayName);
+        redaktrIntent.putExtra(EMAIL, email);
+        redaktrIntent.putExtra(PHONE_NUMBER, phoneNumber);
+        redaktrIntent.putExtra(UID, uId);
         startActivity(redaktrIntent);
 
     }
--- app/src/main/java/edu/unc/cs/haydenl/redaktr/SigninActivity.java
+++ app/src/main/java/edu/unc/cs/haydenl/redaktr/SigninActivity.java
@@ -1,7 +1,75 @@
 package edu.unc.cs.haydenl.redaktr;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SigninActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
+
+    public static final String DISPLAY_NAME = "displayName";
+    public static final String EMAIL = "email";
+    public static final String PHONE_NUMBER = "phoneNumber";
+    public static final String UID = "uId";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        launchLogin();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent intent = new Intent(this, Settings.class);
+                intent.putExtra(DISPLAY_NAME, user.getDisplayName());
+                intent.putExtra(EMAIL, user.getEmail());
+                intent.putExtra(PHONE_NUMBER, user.getPhoneNumber());
+                intent.putExtra(UID, user.getUid());
+                startActivity(intent);
+
+            } else {
+                // TODO should I let them try a couple more times first?
+                finish();
+            }
+
+        }
+    }
+
+    private List<AuthUI.IdpConfig> buildProviders() {
+        return Arrays.asList(
+            new AuthUI.IdpConfig.EmailBuilder().build(),
+//            new AuthUI.IdpConfig.PhoneBuilder().build(),
+            new AuthUI.IdpConfig.GoogleBuilder().build());
+//            new AuthUI.IdpConfig.FacebookBuilder().build(),
+//            new AuthUI.IdpConfig.TwitterBuilder().build());
+    }
+
+    private void launchLogin() {
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(buildProviders())
+                .build(),
+            RC_SIGN_IN);
+    }
 }
