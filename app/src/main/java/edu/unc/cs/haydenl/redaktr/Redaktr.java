63a6e339a26f4267c5b330bd7a4fd98e8bae7a50
hlawler96 <haydenl@live.unc.edu>
2018-09-28 16:00:46 -0400

    welp we tried

--- app/src/main/java/edu/unc/cs/haydenl/redaktr/Redaktr.java
+++ app/src/main/java/edu/unc/cs/haydenl/redaktr/Redaktr.java
@@ -1,8 +1,13 @@
 package edu.unc.cs.haydenl.redaktr;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,6 +17,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -20,6 +27,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
@@ -93,8 +101,16 @@ public class Redaktr extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             try {
+
+
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoUri);
+
+                int rotation = getRotationCompensation(this, this);
+                rotation = ORIENTATIONS.get(rotation);
+                Log.v("DEBUG_TAG", "" + rotation);
+
                 redactedPicture.setImageBitmap(bitmap);
+                redactPicture(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -102,7 +118,6 @@ public class Redaktr extends AppCompatActivity {
     }
 
     private void redactPicture(Bitmap b){
-        redactedPicture.setImageBitmap(b);
         FirebaseApp.initializeApp(this);
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(b);
         FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
@@ -144,6 +159,69 @@ public class Redaktr extends AppCompatActivity {
 
     }
 
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
+    /**
+     * Get the angle by which an image must be rotated given the device's current
+     * orientation.
+     */
+    private int getRotationCompensation( Activity activity, Context context) {
+        try {
+            // Get the device's current rotation relative to its "native" orientation.
+            // Then, from the ORIENTATIONS table, look up the angle the image must be
+            // rotated to compensate for the device's rotation.
+            int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+            int rotationCompensation = ORIENTATIONS.get(deviceRotation);
+
+            // On most devices, the sensor orientation is 90 degrees, but for some
+            // devices it is 270 degrees. For devices with a sensor orientation of
+            // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
+            CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
+            String cameraId = "";
+            for (int i = 0; i < cameraManager.getCameraIdList().length; i++) {
+                if (cameraManager.getCameraCharacteristics(cameraManager.getCameraIdList()[i]).get(CameraCharacteristics.LENS_FACING) ==
+                        CameraCharacteristics.LENS_FACING_BACK) {
+                    cameraId = cameraManager.getCameraIdList()[i];
+                }
+            }
+            int sensorOrientation = cameraManager
+                    .getCameraCharacteristics(cameraId)
+                    .get(CameraCharacteristics.SENSOR_ORIENTATION);
+            rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
+
+            // Return the corresponding FirebaseVisionImageMetadata rotation value.
+            int result;
+            switch (rotationCompensation) {
+                case 0:
+                    result = FirebaseVisionImageMetadata.ROTATION_0;
+                    break;
+                case 90:
+                    result = FirebaseVisionImageMetadata.ROTATION_90;
+                    break;
+                case 180:
+                    result = FirebaseVisionImageMetadata.ROTATION_180;
+                    break;
+                case 270:
+                    result = FirebaseVisionImageMetadata.ROTATION_270;
+                    break;
+                default:
+                    result = FirebaseVisionImageMetadata.ROTATION_0;
+                    Log.e("DEBUG_TAG", "Bad rotation value: " + rotationCompensation);
+            }
+            Log.v("DEBUG_TAG", "" + result);
+            return result;
+        }catch(Exception e){
+            Log.e("DEBUG_TAG", "Bad Camera_Access");
+            return FirebaseVisionImageMetadata.ROTATION_0;
+        }
+    }
 
 
 
